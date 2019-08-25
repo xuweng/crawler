b74046f1cac37bb54ddfd6dd4764b7e83cc73873
xuweng <2028403305@qq.com>
2019-08-25 20:41:15 +0800

    修改加密方式

--- spider/src/main/java/com/crawler/spider/news/ToutiaoApi.java
+++ spider/src/main/java/com/crawler/spider/news/ToutiaoApi.java
@@ -1,17 +1,26 @@
 package com.crawler.spider.news;
 
 import com.crawler.spider.utils.HttpClientUtils;
+import com.crawler.spider.utils.Utils;
 import lombok.Builder;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
 
 @Slf4j
 @Builder
 public class ToutiaoApi {
-    private static final String URL = "https://www.toutiao.com/pgc/ma/?" +
+    private static String URL = "https://www.toutiao.com/pgc/ma/?" +
             "page_type=1&max_behot_time=&uid=50502347096&media_id=50502347096&output=json&" +
-            "is_json=1&count=20&from=user_profile_app&version=2&as=A125DA2BD89A381&cp=5AB81AE3C8116E1&callback=jsonp3";
+            "is_json=1&count=20&from=user_profile_app&version=2&callback=jsonp3";
 
     public String getToutiaoData() {
+        Map<String, String> map = Utils.getToutiaoAsCp();
+
+        String as = map.get("as");
+        String cp = map.get("cp");
+
+        URL += "&as=" + as + "&cp=" + cp;
         return HttpClientUtils.get(URL);
     }
 }
